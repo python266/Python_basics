a73afe15f7c5331d0a607ac31613eab30124d228
Himesh <121535663+python266@users.noreply.github.com>
2023-10-09 23:37:16 +0530

    Add files via upload

---
+++ IF AND ELIF STATEMENT.py
@@ -0,0 +1,21 @@
+"""
+#How to use If and Else
+a = input("Enter the number: ")
+if a == "Avi" or "avi":
+    print("Valid member")
+else:
+    print("Invalid member")
+"""
+#---------------------------------------------
+"""
+#How to use if, elif and else
+import random
+z = int(input("Enter the first number: "))
+if z == 0:
+    print(f"Answer {z*0}")
+elif z <= 100:
+    c = random.randint(1, 100)
+    print(f"Answer {z*c}")
+else:
+    print("Sorry! Greater than 100")
+"""
