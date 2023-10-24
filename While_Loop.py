0ac2800017789617cd2df9bea971fb7d06fce28a
Himesh <121535663+python266@users.noreply.github.com>
2023-10-24 20:17:50 +0530

    Add files via upload

--- While_Loop.py
+++ While_Loop.py
@@ -1,4 +1,15 @@
+"""
+#how to use while loop
 i = 0
 while (i<=15):
     print(i)
     i = i+1
+"""
+
+while True:
+    user_input = int(input("Enter the a number: "))
+    if user_input < 100:
+        continue
+    elif user_input > 100:
+         print("You've congratulation. You've entered more than 100")
+    break
